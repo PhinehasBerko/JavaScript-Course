776ee68c2c42fb72ce9528284197421177cdce18
ZEALOT <phinehasdscience@gmail.com>
2025-06-09 23:14:01 +0000

    include polyfills and transpilers file

---
+++ 18_polyfils_and_transpilers.js
@@ -0,0 +1,11 @@
+// How to make our modern code work on older engines that don’t understand recent features yet?
+
+// There are two tools for that:
+
+// 1. Transpilers.
+// 2. Polyfills.
+
+// Transpilers
+// A transpiler is a special piece of software that translates source code to another 
+// source code. It can parse (“read and understand”) modern code and rewrite 
+// it using older syntax constructs, so that it’ll also work in outdated engines.
